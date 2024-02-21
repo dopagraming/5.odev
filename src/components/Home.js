b8a70f8e38b797bc90a32b9e2b8dec9f58ff929e
dopagraming <dopagraming@gmail.com>
2024-02-21 10:56:43 +0300

    done

---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+const Home = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+  const [password, setPassword] = useState();
+  const [vlaue, setValue] = useState(false);
+  const Navigate = () => {
+    navigate("/about", { state: user });
+  };
+  const uncorrect = () => {
+    return setValue(true);
+  };
+  return (
+    <div className="container">
+      <form>
+        <div>
+          <label>Username</label>
+          <input
+            placeholder="Username"
+            required="required"
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+
+          <input
+            placeholder="Password"
+            required="required"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {vlaue && <div>Password uncorrect</div>}
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              {
+                password == "123456" ? Navigate() : uncorrect();
+              }
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
